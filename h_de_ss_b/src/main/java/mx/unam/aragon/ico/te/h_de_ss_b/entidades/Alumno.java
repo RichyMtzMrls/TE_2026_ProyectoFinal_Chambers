4b09dfc78b65eabc773e34ba0adcdc5ce3de169c
RichyMtzMrls <ricardomartinez321@aragon.unam.mx>
2026-05-11 11:03:41 -0600

    Iniciando la Tabla de Alumno en la carpeta entidades

---
+++ h_de_ss_b/src/main/java/mx/unam/aragon/ico/te/h_de_ss_b/entidades/Alumno.java
@@ -0,0 +1,15 @@
+package mx.unam.aragon.ico.te.h_de_ss_b.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+
+}
