9baad3eed3c6b2837eafa2832d2ba616247c5c6a
RichyMtzMrls <ricardomartinez321@aragon.unam.mx>
2026-05-12 11:15:44 -0600

    Agregando la tabla alumnos de la BD del Backend

--- h_de_ss_b/src/main/java/mx/unam/aragon/ico/te/h_de_ss_b/entidades/Alumno.java
+++ h_de_ss_b/src/main/java/mx/unam/aragon/ico/te/h_de_ss_b/entidades/Alumno.java
@@ -1,15 +1,47 @@
 package mx.unam.aragon.ico.te.h_de_ss_b.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "Alumnos")
+@Data
+@NoArgsConstructor
 public class Alumno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
+    private Long id; // Llave Primaria para los alumnos
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "apellido_paterno", nullable = false, length = 100)
+    private String apellidoPaterno;
+
+    @Column(name = "apellido_materno", nullable = false, length = 100)
+    private String apellidoMaterno;
+
+    @Column(name = "numero_de_cuenta", nullable = false, unique = true, length = 100)
+    private String numeroDeCuenta;
+
+    @Column(nullable = false)
+    private String carrera;
+
+    @Column(name= "correo_electronico", nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String telefono;
+
+    @Column(name = "programa_servicio_social", nullable = false)
+    private String programaServicioSocial;
+
+    @Column(name = "estado_alumno", nullable = false, length = 100)
+    private String estadoAlumno;
+
+    @Column(name = "total_horas")
+    private Integer totalHoras;
+
 
 }
